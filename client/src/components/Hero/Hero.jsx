a5c2f03a4655ea80b65341414fb9c9bb489a7314
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2024-05-08 21:39:18 +0530

    hero page done

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import styles from "./Hero.module.css";
+import heroBgImg from "../../assets/herobg.png";
+import { ComputersCanvas } from "../canvas/index.js";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className={styles["hero-container"]}>
+      <img src={heroBgImg} alt="herobg" />
+      <div className={styles["hero-container-main"]}>
+        <div className={styles["hero-container-sub-one"]}>
+          <div className={styles["sub-one-design"]}>
+            <div className={styles["round"]}></div>
+            <div className={styles["full"]}></div>
+          </div>
+          <div className={styles["about-section"]}>
+            <h2>
+              Hi, I'm <span>Shuvra</span>
+            </h2>
+            <h3>I am a Full Stack Developer</h3>
+            <p>
+              Pursuing B.Tech in Computer Science And Business System from
+              Sister Nivedita University. I am a web Developer .I enjoy creating
+              beautifully designed, intuitive and functional websites. I am also
+              interested in Python Development and Cyber Security
+            </p>
+          </div>
+        </div>
+      </div>
+      <ComputersCanvas />
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
--- client/src/components/canvas/Computers.jsx
+++ client/src/components/canvas/Computers.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
+
   return (
-    <div>Computers</div>
-  )
-}
+    <mesh>
+      <hemisphereLight intensity={2} groundColor="black" />
+      <pointLight intensity={4} />
+      <primitive
+        object={computer.scene}
+        scale={0.75}
+        position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
+  );
+};
 
-export default Computers
+const ComputersCanvas = ({ isMobile }) => {
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <OrbitControls
+        enableZoom={false}
+        maxPolarAngle={Math.PI / 2}
+        minPolarAngle={Math.PI / 2}
+      />
+      <Computers />
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
